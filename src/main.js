0f1f27d5d7a2f626b15e86c255e14c679b1280ec
tytamanik <watislavve@gmail.com>
2025-07-06 00:03:03 +0300

    Added score, interactive UI & choice logic

--- src/main.js
+++ src/main.js
@@ -5,14 +5,24 @@ const topic = document.getElementById('topicInput')
 const difficulty = document.getElementById('difficultySelect')
 const startButton = document.getElementById('startButton')
 const quizContainer = document.getElementById('quizContainer')
-const answersContainer = document.getElementById('answersContainer')
-
 const groq = new Groq({ apiKey: API_KEY, dangerouslyAllowBrowser: true })
-
+const scoreOutput = document.createElement('output')
+let scoreCounter = 0
+let attepmpts = 0
+let currentQuestion = null
+let currentAnswers = null
 startButton.addEventListener('click', e => {
 	e.preventDefault()
+	if (currentQuestion) currentQuestion.remove()
+	if (currentAnswers) currentAnswers.remove()
 	createQuiz()
+	scoreOutput.style.display = 'block'
+	quizContainer.style.display = 'block'
 })
+
+scoreOutput.innerText = `Score: ${scoreCounter} out of ${attepmpts}`
+scoreOutput.classList.add('card__progress')
+quizContainer.append(scoreOutput)
 
 async function createQuiz() {
 	const completion = await groq.chat.completions
@@ -24,28 +34,53 @@ async function createQuiz() {
 {
   "question": "string",
   "options": [
-    "<li class=\\"card__answer\\">...</li>",
-    "<li class=\\"card__answer\\">...</li>",
-    "<li class=\\"card__answer\\">...</li>"
-  ],
-  "correct": "<li class=\\"card__answer\\">...</li>"
+   "<li class=\"card__answer\" data-answer=\"incorrect\">Answer 1</li>",
+  "<li class=\"card__answer\" data-answer=\"correct\">Correct Answer</li>",
+  "<li class=\"card__answer\" data-answer=\"incorrect\">Answer 3</li>"
+  ]
+
 }`,
 				},
 			],
 			model: 'llama-3.3-70b-versatile',
 		})
 		.then(chatCompletion => {
-			startButton.setAttribute('disabled', 'true')
-			console.log(chatCompletion.choices[0].message.content)
 			let response = JSON.parse(chatCompletion.choices[0].message.content)
-			let question = document.createElement('p')
-			question.classList.add('card__question')
-			question.innerHTML = `${response.question}`
-			quizContainer.append(question)
-			let answersContainer = document.createElement('ul')
-			answersContainer.classList.add('card__answers')
-			answersContainer.innerHTML = `${response.options.join('')}`
-			quizContainer.append(answersContainer)
-			console.log(response)
+			startQuiz(response)
 		})
+}
+function startQuiz(response) {
+	startButton.innerHTML = 'Start'
+	startButton.setAttribute('disabled', 'true')
+	let question = document.createElement('p')
+	question.classList.add('card__question')
+	question.textContent = response.question
+	quizContainer.insertBefore(question, scoreOutput)
+	let answersContainer = document.createElement('ul')
+	answersContainer.classList.add('card__answers')
+	answersContainer.innerHTML = response.options.join('')
+	quizContainer.insertBefore(answersContainer, scoreOutput)
+	currentQuestion = question
+	currentAnswers = answersContainer
+	attepmpts++
+	scoreOutput.innerText = `Score: ${scoreCounter} out of ${attepmpts}`
+	let answered = false
+	answersContainer.addEventListener('click', e => {
+		if (answered) return
+		answered = true
+		if (e.target.dataset.answer === 'correct') {
+			scoreCounter++
+			scoreOutput.innerText = `Score: ${scoreCounter} out of ${attepmpts}`
+		}
+		startButton.removeAttribute('disabled')
+		startButton.innerHTML = 'Restart?'
+		const restAnswers = document.querySelectorAll('.card__answer')
+		restAnswers.forEach(answer => {
+			if (answer.dataset.answer === 'correct') {
+				answer.classList.add('card__answer--correct')
+			} else {
+				answer.classList.add('card__answer--wrong')
+			}
+		})
+	})
 }
