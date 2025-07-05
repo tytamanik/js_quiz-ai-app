b72494209ff5d6745ac0b294fc4391dab751621f
tytamanik <watislavve@gmail.com>
2025-07-05 20:03:07 +0300

    Questions generation over API

--- src/main.js
+++ src/main.js
@@ -1,25 +1,51 @@
+import Groq from 'groq-sdk'
 import './style.scss'
-
 const API_KEY = import.meta.env.VITE_GROQ_API_KEY
+const topic = document.getElementById('topicInput')
+const difficulty = document.getElementById('difficultySelect')
+const startButton = document.getElementById('startButton')
+const quizContainer = document.getElementById('quizContainer')
+const answersContainer = document.getElementById('answersContainer')
 
-export async function main() {
-	const chatCompletion = await getGroqChatCompletion()
+const groq = new Groq({ apiKey: API_KEY, dangerouslyAllowBrowser: true })
 
-	// Print the completion returned by the LLM.
+startButton.addEventListener('click', e => {
+	e.preventDefault()
+	createQuiz()
+})
 
-	console.log(chatCompletion.choices[0]?.message?.content || '')
-}
-
-export async function getGroqChatCompletion() {
-	return groq.chat.completions.create({
-		messages: [
-			{
-				role: 'user',
-
-				content: 'Explain the importance of fast language models',
-			},
-		],
-
-		model: 'llama-3.3-70b-versatile',
-	})
+async function createQuiz() {
+	const completion = await groq.chat.completions
+		.create({
+			messages: [
+				{
+					role: 'user',
+					content: `Generate 1 quiz question on topic ${topic.value}, difficulty should be ${difficulty.value}. Return a raw JSON object only. Each option (of total 3) must be a STRING that looks like this: "<li class=\\"card__answer\\">Answer</li>". Do not return HTML nodes. Return ONLY JSON. Example format:
+{
+  "question": "string",
+  "options": [
+    "<li class=\\"card__answer\\">...</li>",
+    "<li class=\\"card__answer\\">...</li>",
+    "<li class=\\"card__answer\\">...</li>"
+  ],
+  "correct": "<li class=\\"card__answer\\">...</li>"
+}`,
+				},
+			],
+			model: 'llama-3.3-70b-versatile',
+		})
+		.then(chatCompletion => {
+			startButton.setAttribute('disabled', 'true')
+			console.log(chatCompletion.choices[0].message.content)
+			let response = JSON.parse(chatCompletion.choices[0].message.content)
+			let question = document.createElement('p')
+			question.classList.add('card__question')
+			question.innerHTML = `${response.question}`
+			quizContainer.append(question)
+			let answersContainer = document.createElement('ul')
+			answersContainer.classList.add('card__answers')
+			answersContainer.innerHTML = `${response.options.join('')}`
+			quizContainer.append(answersContainer)
+			console.log(response)
+		})
 }
